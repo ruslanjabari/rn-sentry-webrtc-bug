792a61979e72affe8eaf3083966493bf21fd3f88
ruslanjabari <rjabari@stanford.edu>
2024-04-27 16:24:04 -0700

    bug

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import Vapi from "@vapi-ai/react-native";
+import * as Sentry from "@sentry/react-native";
 
-export default function App() {
+Sentry.init({
+  dsn: "<YOUR_DSN>",
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+const vapi = new Vapi("<YOUR_API_KEY>");
+
+export default Sentry.wrap(function App() {
+  useEffect(() => {
+    let connect = async () => {
+      vapi.start("<YOUR_ROOM_URL>");
+      vapi.setMuted(false);
+    };
+
+    connect();
+    return () => {
+      vapi.setMuted(true);
+      vapi.stop();
+    };
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
-}
+});
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
